946d908d7f956f8674a25d96db0d5f631253c5b9
Juan Pablo Sosa <jpasosa@gmail.com>
2014-08-01 09:36:11 -0300

    commit inicial, de la rama master.

---
+++ form.php
@@ -0,0 +1,27 @@
+<html>
+
+	<head>
+		<title>Personal INFO</title>
+	</head>
+
+	<body>
+
+		<form method="post" action="<?php echo $PHP_SELF;?>">
+			First Name:<input type="text" size="12" maxlength="12" name="Fname">:<br />
+			Last Name:<input type="text" size="12" maxlength="36" name="Lname">:<br />
+			<input type="submit" value="submit" name="submit"><br />
+		</form>
+
+	</body>
+</html>
+
+
+<?php
+
+if (isset($_POST['Fname'])) {
+	echo 'envio por post';
+} else {
+	echo 'no andaa el post';
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+echo 'Estamos haciendo pruebas interesantes con las ramas en GIT';
+echo '<br />';
+echo 'Ahora estamos en la rama master que es principal; que se genera por defecto.';
+
+
+
+
+
+
+
+
+?>
+
+
